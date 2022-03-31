587e7b4c7dbc2884ab96f2d3d7f67ce8c89c188a
sh0327akg <shota.gk0327m.tf@gmail.com>
2022-03-31 20:17:34 +0900

    修正

--- memo.rb
+++ memo.rb
@@ -6,7 +6,7 @@ memo_type = gets.to_i
 if memo_type == 1
   puts "新規メモの作成"
   puts "拡張子を除いたファイルを入力してください"
-  file_name = gets.to_s 
+  file_name = gets.chomp.to_s 
   puts "メモの内容を入力してください"
   memo = gets.chomp.to_s
 
@@ -17,7 +17,7 @@ if memo_type == 1
 elsif memo_type == 2
   puts "既存のメモを編集します"
   puts "拡張子を除いた編集したいファイル名を入力してください"
-  file_name = gets.to_s
+  file_name = gets.chomp.to_s
   puts "メモの内容を編集してください"
   memo = gets.chomp.to_s
 
